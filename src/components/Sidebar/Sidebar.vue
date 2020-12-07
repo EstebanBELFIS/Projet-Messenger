ca9bf56c0733b6e14c4377003bdc095bcdf961bb
EstebanBELFIS <57663705+EstebanBELFIS@users.noreply.github.com>
2020-12-07 17:11:43 +0100

    Sidebar V2

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -49,13 +49,25 @@
         </div>
       </div>
       <div v-for="conversation in conversations" :key="conversation.id">
-        <div class="conversation new" title="bob" @click="openConversation(conversation.id)">
+        <div v-if="conversation.participants.length >2" class="conversation new"  @click="openConversation(conversation.id)">
+          <a class="avatar">
+            <i class="users icon"></i>
+          </a>
+          <div class="content">
+            <div class="metadata">
+              <div class="title"><i class="ui small icon circle"> </i>Groupe : <span v-for="participant in conversation.participants" :key="participant"> {{participant}},</span></div>
+              <span class="time">01:30:58</span>
+            </div>
+            <div class="text">C'est vraiment super Alice !</div>
+          </div>
+        </div>
+        <div v-if="conversation.participants.length <=2" class="conversation new"  @click="openConversation(conversation.id)">
           <a class="avatar">
             <img src="https://source.unsplash.com/7omHUGhhmZ0/100x100" />
           </a>
           <div class="content">
             <div class="metadata">
-              <div class="title"><i class="ui small icon circle"> </i>Groupe : <span v-for="participant in conversation.participants" :key="participant"> {{participant}},</span></div>
+              <div class="title"><i class="ui small icon circle"> </i>{{conversation.participants[1]}}</div>
               <span class="time">01:30:58</span>
             </div>
             <div class="text">C'est vraiment super Alice !</div>
